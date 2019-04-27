a5d0ac02a07d9ae8bf6370e00b45d93c6ec4d022
Henrique Sander Fernandes <hqsander@gmail.com>
2019-04-27 05:18:39 -0300

    Inclusão de ícones nas abas.

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,8 +1,23 @@
+import React from 'react';
 import { createAppContainer, createSwitchNavigator, createBottomTabNavigator } from 'react-navigation';
+import Icon from 'react-native-ionicons';
 
 import Login from './telas/Login';
 import ExemploA from './telas/ExemploA';
 import ExemploB from './telas/ExemploB';
+
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  const IconComponent = Icon;
+  let iconName;
+  if (routeName === 'ExemploA') {
+    iconName = 'ios-information-circle';
+  } else if (routeName === 'ExemploB') {
+    iconName = 'ios-options';
+  }
+
+  return <IconComponent name={iconName} size={25} color={tintColor} />;
+};
 
 const Navigator = createAppContainer(
   createSwitchNavigator({
@@ -10,6 +25,11 @@ const Navigator = createAppContainer(
     App: createBottomTabNavigator({
       ExemploA,
       ExemploB
+    },
+    {
+      defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, tintColor }) => getTabBarIcon(navigation, focused, tintColor),
+      })
     })
   })
 );
